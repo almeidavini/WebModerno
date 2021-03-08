798092cb7420a43c97c1e6144d3eb026f2ee5521
almeidasilva <vinicius.almeida@serget.com.br>
2021-03-08 18:51:30 -0300

    Adicionando os arquivos de aula

--- Fundamentos/funcao1.js
+++ Fundamentos/funcao1.js
@@ -13,3 +13,12 @@ imprimirSoma(1)
 // Ao chamar a função passando mais parametros do que é exigido, o JS apenas ignora. Outras linguagens gerariam erro.
 
 imprimirSoma(1, 10, 20, 30, 40)
+
+// Função com retorno
+// É possível definir um valor padrão para o argumento de uma função
+function soma(a = 0, b = 1){
+    return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(2))
